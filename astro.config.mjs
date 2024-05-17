ccece4eb8424e2f8f7dfa2ad111995e26fef493d
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-05-17 08:57:16 +0200

    start

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), mdx(), sitemap(), tailwind()]
+});
